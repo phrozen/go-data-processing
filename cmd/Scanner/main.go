c38036bef29e837c094a16b63a8e88cf91b2d6e0
Guillermo Estrada <phrzn@live.com>
2022-01-27 21:03:39 -0600

    Enabled Scanner version to process

--- cmd/Scanner/main.go
+++ cmd/Scanner/main.go
@@ -4,30 +4,30 @@ import (
 	"bufio"
 	"log"
 	"os"
+
+	"github.com/phrozen/go-data-processing/pkg/counter"
 )
 
 func main() {
 	// Open file, it's an io.Reader
-	file, err := os.Open("file.txt")
+	file, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 	}
 	// Close it when we are done
 	defer file.Close()
+	// Create counter
+	count := counter.NewCounter()
 	// Create a new buffered scanner from file
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		// Process each line from the buffer
-		ProcessLine(scanner.Text())
+		count.ProcessLine(scanner.Text())
 	}
 	// Check the error of the scanner if any
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-var count uint64
-
-func ProcessLine(line string) {
-	count++
+	// Results
+	count.PrintResult()
 }
